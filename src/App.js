92ade87e5d33e9fb21f1e36616ab2071da5b704d
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 14:11:11 +0200

    added NASA CME API url and key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import SingUp from "./Components/Auth/SingUp";
+import CoronalMassInjection from "./Components/APIs/SpaceWeather/CoronalMassInjection";
 
 function App() {
-  return <div className="App">
-    <SingUp />
-  </div>;
+  return (
+    <div className='App'>
+      <SingUp />
+      <CoronalMassInjection />
+    </div>
+  );
 }
 
 export default App;
