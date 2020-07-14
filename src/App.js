3b91f128b5adf5e70295d1dde0c09f7b993f0e64
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 20:04:52 +0200

    removed unused variables in App.js and test component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
 import "./App.css";
-import SingUp from "./Components/Auth/SingUp";
-import CoronalMassInjection from "./Components/APIs/SpaceWeather/CoronalMassInjection";
-import SolarFlares from "./Components/APIs/SpaceWeather/SolarFlares";
-import GeomagnaticStorm from "./Components/APIs/SpaceWeather/GeomagnaticStorm";
 
 function App() {
-  return (
-    <div className='App'>
-      <GeomagnaticStorm />
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
