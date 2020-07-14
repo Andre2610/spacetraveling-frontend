68afb6a2d2ca4a2f7afe48626ac2ebde8e7d3364
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 15:33:17 +0200

    created solarFlare reducerSlice | redux store files created: actions / reducer / selector

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import SingUp from "./Components/Auth/SingUp";
 import CoronalMassInjection from "./Components/APIs/SpaceWeather/CoronalMassInjection";
+import SolarFlares from "./Components/APIs/SpaceWeather/SolarFlares";
 
 function App() {
   return (
     <div className='App'>
-      <SingUp />
-      <CoronalMassInjection />
+      <SolarFlares />
     </div>
   );
 }
