0fac9c6119a5829f4218d723063167cf725f5aef
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 16:08:18 +0200

    render solar flare API data on component

--- src/Components/APIs/SpaceWeather/SolarFlares.js
+++ src/Components/APIs/SpaceWeather/SolarFlares.js
@@ -7,14 +7,34 @@ export default function SolarFlares() {
   const dispatch = useDispatch();
   const solarFlare = useSelector(selectSolarFlare);
 
+  function renderSolarFlare() {
+    if (!solarFlare) {
+      return <p>No Solar Flares were sighted in the last 30 days</p>;
+    }
+    {
+      return (
+        <>
+          <p>
+            A class {solarFlare.classType} solar flare has been recorded in our
+            solar system.
+          </p>
+          <p>
+            Its begintime was: {solarFlare.beginTime} and it peaked at:{" "}
+            {solarFlare.peakTime}
+          </p>
+        </>
+      );
+    }
+  }
+
   // FLR = Solar Flare - type of space weather
   useEffect(() => {
     dispatch(getFLR());
-  });
-
+  }, []);
   return (
     <div>
       <h3>Solar Flares</h3>
+      {renderSolarFlare()}
     </div>
   );
 }
