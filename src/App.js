3725db9e57e5b412cfe5a39d8dc950a7ccd8113c
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 21:32:55 +0200

    updating files to latest state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import CoronalMassInjection from "./Components/APIs/SpaceWeather/CoronalMassInjection";
 import SolarFlares from "./Components/APIs/SpaceWeather/SolarFlares";
 import AuthModal from "./Components/Auth/AuthModal";
 import SelectDestinationForm from "./Components/selectorForm";
+import { useDispatch } from "react-redux";
 
 function App() {
   return (
