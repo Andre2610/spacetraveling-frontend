bd527546ad332e8cd9aaa5b82f55bc2112fbb106
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 13:49:56 +0200

    added CME redux reducer / action file / selector

--- src/Components/APIs/SpaceWeather/CoronalMassInjection.js
+++ src/Components/APIs/SpaceWeather/CoronalMassInjection.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
 
 export default function CoronalMassInjection() {
     const dispatch = useDispatch()
+
+
+    //CME = coronal Mass Injection - type of spaceWeather
+    useEffect(() => {
+        dispatch(getCME())
+    })
 
     return (
         <div>
