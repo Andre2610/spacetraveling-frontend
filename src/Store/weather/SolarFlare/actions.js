3f47b243df27312b788ee1fb8798a36cd6385336
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 15:38:40 +0200

    added NASA solar Flare API url

---
+++ src/Store/weather/SolarFlare/actions.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import NASAK from "../../../Config/constants";
+
+export function getFLR() {
+  return async (dispatch, getState) => {
+    console.log("works?");
+  };
+}
