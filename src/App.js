022956a23d44d87561843263e81d770d01705e91
Sebastian Baez <sdbb.unl@gmail.com>
2020-07-14 23:28:45 +0200

    Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./App.css";
-import SingUp from "./Components/Auth/SingUp";
+import { Switch, Route } from "react-router-dom";
+import Navigation from "./Components/UI/Navigation";
+import Footer from "./Components/Footer/";
+import Homepage from "./Pages/Homepage";
+import Booking from "./Pages/Booking";
 
 function App() {
-  return <div className="App">
-    <SingUp />
-  </div>;
+  return (
+    <div className="App">
+      <div className="Header">
+        <Navigation />
+      </div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/booking" component={Booking} />
+      </Switch>
+      <div className="Footer"></div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
