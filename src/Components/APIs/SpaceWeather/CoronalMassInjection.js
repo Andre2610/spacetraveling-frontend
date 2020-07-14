f32db28262c28f02b09d1941bf4565ced266fdba
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 14:11:11 +0200

    added NASA CME API url and key

--- src/Components/APIs/SpaceWeather/CoronalMassInjection.js
+++ src/Components/APIs/SpaceWeather/CoronalMassInjection.js
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { getCME } from "../../../Store/weather/CoronalMassInjection/actions";
 
 export default function CoronalMassInjection() {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
+  //CME = coronal Mass Injection - type of spaceWeather
+  useEffect(() => {
+    dispatch(getCME());
+  });
 
-    //CME = coronal Mass Injection - type of spaceWeather
-    useEffect(() => {
-        dispatch(getCME())
-    })
-
-    return (
-        <div>
-            <h1>SpaceWeather wedge component</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>SpaceWeather wedge component</h1>
+    </div>
+  );
 }
---
+++ src/Store/weather/CoronalMassInjection/actions.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import NASA_CME_URL from "../../../Config/constants";
+
+export function getCME() {
+  return async () => {
+    console.log("working?");
+  };
+}
