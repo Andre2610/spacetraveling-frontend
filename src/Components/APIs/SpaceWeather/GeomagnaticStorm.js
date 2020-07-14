49a7ca57c4091421d1d4d3ab599a500bd50a89ea
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 20:04:52 +0200

    removed unused variables in App.js and test component

--- src/Components/APIs/SpaceWeather/GeomagnaticStorm.js
+++ src/Components/APIs/SpaceWeather/GeomagnaticStorm.js
@@ -5,6 +5,7 @@ import { getGST } from "../../../Store/weather/GeomagnaticStorm/actions";
 export default function SolarFlares() {
   const dispatch = useDispatch();
 
+  // No GST Storm in last 3 years -> consider updating / rendering data when new GST has been recorded to display some data.
   // GST = Solar Flare - type of space weather
   useEffect(() => {
     dispatch(getGST());
