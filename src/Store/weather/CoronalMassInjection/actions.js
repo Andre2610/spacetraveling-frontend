155ab131d6672c98da07aa64a4362a0a572df1d1
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 14:34:23 +0200

    dispatched GET call to NASA DIONK API and send res.data to reducer slice

--- src/Store/weather/CoronalMassInjection/actions.js
+++ src/Store/weather/CoronalMassInjection/actions.js
@@ -1,8 +1,23 @@
 import axios from "axios";
-import NASA_CME_URL from "../../../Config/constants";
+import { NASA_CME_URL, NASAK } from "../../../Config/constants";
+
+export const FETCHED_CME_INFO = "FETCHED_CME_INFO";
+
+export function fetchedCMEInfo(data) {
+  return {
+    type: FETCHED_CME_INFO,
+    payload: data,
+  };
+}
 
 export function getCME() {
-  return async () => {
-    console.log("working?");
+  return async (dispatch, getState) => {
+    try {
+      const res = await axios.get(`${NASA_CME_URL}&api_key=${NASAK}`);
+      //   console.log("whats the result?", res.data);
+      dispatch(fetchedCMEInfo(res.data));
+    } catch (e) {
+      console.log(e);
+    }
   };
 }
