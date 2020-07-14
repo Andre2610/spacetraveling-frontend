cdc2850c3670fa01383514665c35fe3d008f4c8c
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 12:15:31 +0200

    added controlled input fields and eventlisteners

--- src/Components/Auth/SingUp.js
+++ src/Components/Auth/SingUp.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 
 export default function SingUp() {
-    const [firstName, setfirstName] = useState()
-    const [lastName, setLastName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+    const [firstName, setfirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
+    function submitForm(event) {
+        event.preventDefault()
 
+        setfirstName("")
+        setLastName("")
+        setEmail("")
+        setPassword("")
+    }
 
     return (
         <div>
@@ -14,28 +21,33 @@ export default function SingUp() {
                 <div>
                     <input
                         value={firstName}
+                        onChange={event => setfirstName(event.target.value)}
                         type="text"
                         placeholder="please enter your first name"
                         required
                     />
                     <input
                         value={lastName}
+                        onChange={event => setLastName(event.target.value)}
                         type="text"
                         placeholder="please enter your last name"
                         required
                     />
                     <input
                         value={email}
+                        onChange={event => setEmail(event.target.value)}
                         type="email"
                         placeholder="please enter your email"
                         required
                     />
                     <input
                         value={password}
+                        onChange={event => setPassword(event.target.value)}
                         type="password"
                         placeholder
                         required
                     />
+                    <button type="submit"> Sign Up</button>
                 </div>
             </form>
         </div>
