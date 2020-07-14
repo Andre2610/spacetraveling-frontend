d63443b1fa5e5bdf7816fee75d2c3d57d0864b10
Andre2610 <dre2610@gmail.com>
2020-07-14 16:34:54 +0200

    added token still valid action and reducer

--- src/Store/user/actions.js
+++ src/Store/user/actions.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { URL } from "../../Config/constants";
+import { selectToken } from "./selectors";
 
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const TOKEN_STILL_VALID = "TOKEN_STILL_VALID";
+export const LOG_OUT = "LOG_OUT";
 
 const loginSuccess = (userWithToken) => {
   return {
@@ -9,6 +12,13 @@ const loginSuccess = (userWithToken) => {
     payload: userWithToken,
   };
 };
+
+const tokenStillValid = (userWithoutToken) => ({
+  type: TOKEN_STILL_VALID,
+  payload: userWithoutToken,
+});
+
+export const logOut = () => ({ type: LOG_OUT });
 
 export const login = (credentials) => {
   return async (dispatch, getState) => {
@@ -31,6 +41,32 @@ export const login = (credentials) => {
         //   dispatch(setMessage("danger", true, error.message));
       }
       // dispatch(appDoneLoading());
+    }
+  };
+};
+
+export const getUserWithStoredToken = () => {
+  return async (dispatch, getState) => {
+    const token = selectToken(getState());
+
+    if (token === null) return;
+
+    // dispatch(appLoading());
+    try {
+      const response = await axios.get(`${URL}/me`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      dispatch(tokenStillValid(response.data));
+      //   dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.message);
+      } else {
+        console.log(error);
+      }
+      dispatch(logOut());
+      //   dispatch(appDoneLoading());
     }
   };
 };
