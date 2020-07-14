a8f0638095c74968f7dc620440919a61d2f069c1
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-14 11:12:16 +0200

    added store index

--- src/components/APIs/SpaceWeather/CoronalMassInjection.js
+++ src/components/APIs/SpaceWeather/CoronalMassInjection.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function CoronalMassInjection() {
+    const dispatch = useDispatch()
+
     return (
         <div>
-
+            <h1>SpaceWeather wedge component</h1>
         </div>
     )
 }
