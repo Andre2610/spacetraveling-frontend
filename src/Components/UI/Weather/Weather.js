5af3e8970db605b4636472877d729fe69de3df10
Sebastian Baez <sdbb.unl@gmail.com>
2020-07-15 15:01:40 +0200

    Carousel Shell with Test

--- src/Components/UI/Weather/Weather.js
+++ src/Components/UI/Weather/Weather.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import Image from "../../../Images/cme.gif";
 import "./Weather.css";
 export default function Weather(props) {
   const { startTime, note, sourceLocation, link } = props.objectToShow;
   return (
-    <div>
-      <img src={Image} style={{ width: "100%" }} />
-      <h5>Start Time: </h5>
-      <p>{startTime}</p>
-      <h5>Source: </h5>
-      <p>{sourceLocation}</p>
-      <h5>Notes: </h5>
-      <p>{note}</p>
-      <p>Link</p>
+    <div className="WeatherContainer">
+      <Card>
+        <Card.Img variant="top" src={Image} />
+        <Card.Body>
+          <Card.Title>Weather</Card.Title>
+          <Card.Text>{note}</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Start Time: {startTime}</ListGroupItem>
+          <ListGroupItem>Source Location: {sourceLocation}</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href={link}>More info</Card.Link>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
