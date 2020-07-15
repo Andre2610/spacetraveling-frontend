bc02f30e10800eb13b247e621dc592aef425a080
Andre2610 <dre2610@gmail.com>
2020-07-16 00:20:28 +0200

    dark mode toogle implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import Navigation from "./Components/UI/Navigation";
 import Navbar from "./Components/UI/Navigation/Navbar";
 import Footer from "./Components/Footer/Footer";
 import Homepage from "./Pages/Homepage/Homepage";
@@ -11,30 +10,32 @@ import {
   Container,
   CssBaseline,
   ThemeProvider,
-  createMuiTheme,
+  Paper,
 } from "@material-ui/core";
 import CoronalMassInjection from "./Components/APIs/SpaceWeather/CoronalMassInjection";
 import SolarFlares from "./Components/APIs/SpaceWeather/SolarFlares";
 
 function App() {
-  const [darkMode, set_darkMode] = useState(false);
-  const darkModeToggleTheme = createMuiTheme({
-    palette: {
-      type: darkMode ? "dark" : "light",
-    },
-  });
-
+  const [darkMode, set_darkMode] = useState(true);
+  console.log("is it on", darkMode);
+  const darkModeToggleTheme = {
+    ...theme,
+    palette: { ...theme.palette, type: darkMode ? "dark" : "light" },
+  };
+  console.log("my theme", darkModeToggleTheme);
   return (
-    <ThemeProvider theme={theme}>
-      <Container disableGutters="true" maxWidth="100vw">
-        <CssBaseline />
-        <Navbar set_darkMode={set_darkMode} />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/booking" component={Booking} />
-        </Switch>
-        <Footer />
-      </Container>
+    <ThemeProvider theme={darkModeToggleTheme}>
+      <Paper>
+        <Container disableGutters="true" maxWidth="100vw">
+          <CssBaseline />
+          <Navbar darkMode={darkMode} set_darkMode={set_darkMode} />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/booking" component={Booking} />
+          </Switch>
+          <Footer />
+        </Container>
+      </Paper>
     </ThemeProvider>
   );
 }
