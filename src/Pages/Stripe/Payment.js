ad6a158308d9a6974aa7b1819c3a9885227d7ec5
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-15 23:32:02 +0200

    added stripe function and fetch in payment page

--- src/Pages/Stripe/Payment.js
+++ src/Pages/Stripe/Payment.js
@@ -9,10 +9,37 @@ export default function Payment() {
     product: "trip",
   });
 
+  const paymentToken = (token) => {
+    const body = {
+      token,
+      product,
+    };
+    const headers = {
+      "Content-type": "application/json",
+    };
+
+    return fetch(`http://localhost:5000/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        console.log("RESPONSE", response);
+        const { status } = response;
+        console.log("STATUS", status);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <>
       <p>I AM THE STRIPE component</p>
-      <StripeCheckout stripeKey='' token='' name='Trip Name insert HERE'>
+      <StripeCheckout
+        stripeKey={process.env.REACT_APP_KEY}
+        token={paymentToken}
+        name='Trip Name insert HERE'
+        amount={product.price * 100}
+      >
         <Button variant='contained' color='secondary'>
           Book my ticket!
         </Button>
