e0edeb5d7a1a6ec84588dbd73fafb869cb7047a5
Andre2610 <dre2610@gmail.com>
2020-07-15 10:46:43 +0200

    added log out button with conditions to show it

--- src/Components/UI/Navigation/index.js
+++ src/Components/UI/Navigation/index.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
 import NavbarItem from "./NavbarItem";
+import AuthModal from "../../Auth/AuthModal";
+import Logout from "../../Auth/Logout";
+import { selectToken, selectUser } from "../../../Store/user/selectors";
 // import LogonFocus from "../../svg/nFocusLogoBar.svg";
 
 export default function Navigation() {
+  const token = useSelector(selectToken);
+
   return (
     <Navbar bg="dark" expand="lg">
       <Navbar.Brand as={NavLink} to="/">
@@ -16,6 +22,7 @@ export default function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
           <NavbarItem path="/" linkText="Placeholder" />
+          {token ? <Logout /> : <AuthModal />}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
