390972022054672d727c770f167608a75bf6c8e1
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-15 22:52:38 +0200

    added checkout- page and Route

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ import Homepage from "./Pages/Homepage/Homepage";
 import Booking from "./Pages/Booking/Booking";
 import CoronalMassInjection from "./Components/APIs/SpaceWeather/CoronalMassInjection";
 import SolarFlares from "./Components/APIs/SpaceWeather/SolarFlares";
+import Payment from "./Pages/Stripe/Payment";
 
 function App() {
   return (
-    <div className="App">
-      <div className="Header">
+    <div className='App'>
+      <div className='Header'>
         <Navigation />
       </div>
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/booking" component={Booking} />
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/booking' component={Booking} />
+        <Route exact path='/booking/checkout' component={Payment} />
       </Switch>
-      <div className="Footer"></div>
+      <div className='Footer'></div>
       <Footer />
-
     </div>
   );
 }
