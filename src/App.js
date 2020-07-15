ee0cf220495761dc206294fe26e0e3eeb30725e5
Sebastian Baez <sdbb.unl@gmail.com>
2020-07-15 09:44:38 +0200

    File names changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Navigation from "./Components/UI/Navigation";
-import Footer from "./Components/Footer/";
-import Homepage from "./Pages/Homepage";
-import Booking from "./Pages/Booking";
+import Footer from "./Components/Footer/Footer";
+import Homepage from "./Pages/Homepage/Homepage";
+import Booking from "./Pages/Booking/Booking";
 import CoronalMassInjection from "./Components/APIs/SpaceWeather/CoronalMassInjection";
 import SolarFlares from "./Components/APIs/SpaceWeather/SolarFlares";
-
 
 function App() {
   return (
