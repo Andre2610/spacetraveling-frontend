75c15e8edae3ce374c0e7543185f6a1bc8b9669d
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-15 23:05:36 +0200

    barebone Stripe structure

--- src/Pages/Stripe/Payment.js
+++ src/Pages/Stripe/Payment.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { Button } from "@material-ui/core";
 
 export default function Payment() {
-  return <div>I AM THE STRIPE component</div>;
+  const [product, setProduct] = useState({
+    name: "",
+    price: 5,
+    product: "trip",
+  });
+
+  return (
+    <>
+      <p>I AM THE STRIPE component</p>
+      <StripeCheckout stripeKey='' token='' name='Trip Name insert HERE'>
+        <Button variant='contained' color='secondary'>
+          Book my ticket!
+        </Button>
+      </StripeCheckout>
+    </>
+  );
 }
