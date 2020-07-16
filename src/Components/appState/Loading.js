e69379cae04b683e8b715e131a6e3dc5e04695c8
Andre2610 <dre2610@gmail.com>
2020-07-16 23:40:01 +0200

    loading component created

--- src/Components/appState/Loading.js
+++ src/Components/appState/Loading.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Spinner from "react-bootstrap/Spinner";
-import "./spinner.css";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function Loading() {
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
+
+export default function Loading(props) {
+  const classes = useStyles();
+
   return (
-    <div className="loading_spinner">
-      <Spinner animation="border" role="status">
-        <span className="sr-only">Loading...</span>
-      </Spinner>
-    </div>
+    <Backdrop className={classes.backdrop} open={props.open}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
   );
 }
