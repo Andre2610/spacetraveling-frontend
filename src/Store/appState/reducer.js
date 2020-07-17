1388b9ed1e4f7ee0f9048b124dacc625f91ae32a
Andre2610 <dre2610@gmail.com>
2020-07-17 13:01:28 +0200

    added isAdmin to signup form

--- src/Store/appState/reducer.js
+++ src/Store/appState/reducer.js
@@ -19,7 +19,6 @@ export default (state = initialState, action) => {
       return { ...state, loading: false };
 
     case SET_MESSAGE:
-      console.log("In the reducer");
       return { ...state, message: action.payload };
 
     case CLEAR_MESSAGE:
