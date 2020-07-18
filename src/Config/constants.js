f2a1f9f8bf1208d548e0ed8db9bdbc1808bd0a43
Andre2610 <dre2610@gmail.com>
2020-07-18 19:11:10 +0200

    agasg

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,7 +1,7 @@
 export const URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const NASAK = process.env.REACT_APP_NASAK;
-
+// Will this work?
 //NASA API URL collection
 export const NASA_CME_URL = process.env.REACT_APP_NASA_CME_URL;
 export const NASA_FLR_URL = process.env.REACT_APP_NASA_FLR_URL;
